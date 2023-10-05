9f09ada730ee453b451fefbdecaf0311060bef1d
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-06 00:53:46 +0530

    Add method for comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+
+  after_create :update_comment_counter_post
+
+  def author=(user)
+    self.user_id = user.id
+  end
+
+  def update_comment_counter_post
+    post.update(comments_counter: post.comments.count)
+  end
 end

7f47b4832e075d3dd6e1fb31a72541f2e463a5de
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-07 04:05:08 +0530

    Update app/models/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
 
   after_save :update_posts_counter_user
+  after_initialize :set_counters_to_zero
+
+  def set_counters_to_zero
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
+  end
 
   def update_posts_counter_user
     author.update(post_counter: author.posts.count)

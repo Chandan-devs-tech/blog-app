a55588967409d800c18cd7d712899442e24c220e
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-14 00:50:29 +0530

    Modify app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end

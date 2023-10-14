efea81cc001b7a01c393322b0dac7efe5e3dbd69
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-14 18:31:01 +0530

    Modify config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] 
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
   root "users#index"
 end

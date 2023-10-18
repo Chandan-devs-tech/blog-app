227e3f056f4f822fe6c9f03d11d5b63e782ef45d
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-19 03:16:26 +0430

    Update  app/controllers/users_controller.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def sign_out_user
+    sign_out(current_user)
+    redirect_to root_path, notice: 'Signed out successfully'
+  end
 end

ef378d07b3020152ba05cad95a08281bbdceb625
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-21 02:28:34 +0430

    Update app/controllers/comments_controller.rb file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,19 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @user = current_user
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    if @comment.user == @user || @user.role == 'admin'
+      @comment.destroy
+      flash[:notice] = 'Comment successfully deleted.'
+    else
+      flash[:alert] = 'You are not authorized to delete this comment.'
+    end
+    redirect_to user_post_path(@post.author_id, @post.id)
+  end
+
   private
 
   def comment_parameters

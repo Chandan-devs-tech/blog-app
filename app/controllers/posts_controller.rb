8745dab3e8c069f9ab64585eec545526c12b9bd3
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-21 02:28:13 +0430

    Update  app/controllers/posts_controller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @user = current_user
+    @post = Post.includes(:likes).find(params[:id])
+    if @post.author == @user || @user.role == 'admin'
+      @post.likes.destroy_all
+      @post.comments.destroy_all
+      @post.destroy!
+      flash[:notice] = 'Post successfully deleted.'
+    else
+      flash[:alert] = 'You are not authorized to delete this Post.'
+    end
+    redirect_to user_posts_path(id: @user.id)
+  end
+
   private
 
   def post_parameters

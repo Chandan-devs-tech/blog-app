c4f867715d6b454636de75f14fa0f2a6d472072c
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-21 03:18:02 +0530

    Updated config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
   devise_for :users
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
